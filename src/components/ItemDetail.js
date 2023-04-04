310fc39190805844c37bd4aa8422b79d9a1f558f
JoannaKowalik <joanna.kowalik@icloud.com>
2023-04-04 11:07:41 +0200

    recommended items fetching, not displaying

--- src/components/ItemDetail.js
+++ src/components/ItemDetail.js
@@ -8,16 +8,17 @@ import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import { Box, Typography, Button } from "@mui/material";
-// import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
 import LikeButton from "./LikeButton";
 
 const ItemDetail = () => {
   const { id } = useParams();
+  const { categoryId } = useParams();
   const [item, setItem] = useState(null);
   const [distance, setDistance] = useState(null);
   const [lat, setLat] = useState(null);
   const [lng, setLng] = useState(null);
-  // const [like, setLike] = useState(0);
+  const [chosenCategory, setChosenCategory] = useState(null);
+
   const navigate = useNavigate();
 
   // Fetching item with specific id
@@ -48,6 +49,21 @@ const ItemDetail = () => {
         });
     }
   }, [id, lat, lng]);
+
+  useEffect(() => {
+    fetch(
+      `https://geofree.pythonanywhere.com/api/ml-ranking/?chosen_category=${categoryId}`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setChosenCategory(data);
+        console.log("recommended", data);
+      })
+      .catch((e) => {
+        console.log("ERROR", e);
+      });
+  }, [categoryId]);
 
   // If unavailable
 
@@ -125,122 +141,141 @@ const ItemDetail = () => {
 
   return (
     <div>
-      {/* Like button */}
-      <Box
-        sx={{
-          display: "flex",
-          flexDirection: "row-reverse",
-          position: "relative",
-        }}
-      >
+      <div>
+        {/* Like button */}
         <Box
           sx={{
-            position: "absolute",
-
-            justifyContent: "flex-end",
-            top: "-1px",
-            width: "45px",
-            zIndex: "999",
+            display: "flex",
+            flexDirection: "row-reverse",
+            position: "relative",
           }}
         >
-          <LikeButton id={id} disabled={!isAvailable} />
+          <Box
+            sx={{
+              position: "absolute",
+
+              justifyContent: "flex-end",
+              top: "-1px",
+              width: "45px",
+              zIndex: "999",
+            }}
+          >
+            <LikeButton id={id} disabled={!isAvailable} />
+          </Box>
         </Box>
-      </Box>
-      {/* Image slider */}
-      <Box>
-        <Slider
-          {...settings}
-          prevArrow={<PrevArrow />}
-          nextArrow={<NextArrow />}
-        >
-          {item.images.map((image, index) => (
-            <Box
-              key={index}
-              component="img"
-              sx={{
-                height: 400,
-                filter: !isAvailable ? "" : "grayscale(100%)",
-                border: 1,
-                borderColor: "border.main",
-                objectFit: "cover",
-                display: "block",
-              }}
-              alt="donated items"
-              src={`https://geofree.pythonanywhere.com${image.image}`}
-              onError={onError}
-            />
-          ))}
-        </Slider>
-        <Box position="absolute" top={65} left={0} sx={{}}>
-          {availability()}
+        {/* Image slider */}
+        <Box>
+          <Slider
+            {...settings}
+            prevArrow={<PrevArrow />}
+            nextArrow={<NextArrow />}
+          >
+            {item.images.map((image, index) => (
+              <Box
+                key={index}
+                component="img"
+                sx={{
+                  height: 400,
+                  filter: !isAvailable ? "" : "grayscale(100%)",
+                  border: 1,
+                  borderColor: "border.main",
+                  objectFit: "cover",
+                  display: "block",
+                }}
+                alt="donated items"
+                src={`https://geofree.pythonanywhere.com${image.image}`}
+                onError={onError}
+              />
+            ))}
+          </Slider>
+          <Box position="absolute" top={65} left={0} sx={{}}>
+            {availability()}
+          </Box>
         </Box>
-      </Box>
 
-      {/* Item information */}
+        {/* Item information */}
 
-      <Box sx={{ p: 5, flexDirection: "column" }} width="100%">
-        <div key={item.id}>
-          <Typography variant="h5" sx={{ fontWeight: "bold", mb: 1 }}>
-            {item.title}
-          </Typography>
-          <Typography variant="h6" sx={{ fontWeight: "bold", mb: 1 }}>
-            Description
-          </Typography>
-          <Typography variant="body1" sx={{ mb: 1 }}>
-            {item.description}
-          </Typography>
-
-          <Typography variant="body1" sx={{ mb: 1 }}>
-            <strong>Condition:</strong> {item.condition}
-          </Typography>
-          <Typography variant="body1">
-            Posted {Array.from(item.item_age)[0]} days ago
-          </Typography>
-
-          {distance ? (
-            <Typography variant="body1">
-              <strong>Distance:</strong>
-              {distance.toLocaleString("en-US", { maximumFractionDigits: 1 })}
-              km
+        <Box sx={{ p: 5, flexDirection: "column" }} width="100%">
+          <div key={item.id}>
+            <Typography variant="h5" sx={{ fontWeight: "bold", mb: 1 }}>
+              {item.title}
             </Typography>
-          ) : (
-            <Typography variant="body1">Loading distance...</Typography>
-          )}
-          <Location
-            hidden
-            setLat={setLat}
-            setLng={setLng}
-            lat={lat}
-            lng={lng}
-          />
-          <Button
-            variant="contained"
-            color="secondary"
-            sx={{
-              m: 1,
-              height: "40px",
-              width: "45%",
-            }}
-          >
-            Mark as taken
-          </Button>
+            <Typography variant="h6" sx={{ fontWeight: "bold", mb: 1 }}>
+              Description
+            </Typography>
+            <Typography variant="body1" sx={{ mb: 1 }}>
+              {item.description}
+            </Typography>
 
-          <Button
-            variant="contained"
-            color="secondary"
-            onClick={navigateToItem}
-            // component={RouterLink}
-            // to="/map"
-            sx={{
-              m: 1,
-              height: "40px",
-              width: "45%",
-            }}
-          >
-            Show on map
-          </Button>
-        </div>
-      </Box>
+            <Typography variant="body1" sx={{ mb: 1 }}>
+              <strong>Condition:</strong> {item.condition}
+            </Typography>
+            <Typography variant="body1">
+              Posted {Array.from(item.item_age)[0]} days ago
+            </Typography>
+
+            {distance ? (
+              <Typography variant="body1">
+                <strong>Distance:</strong>
+                {distance.toLocaleString("en-US", { maximumFractionDigits: 1 })}
+                km
+              </Typography>
+            ) : (
+              <Typography variant="body1">Loading distance...</Typography>
+            )}
+            <Location
+              hidden
+              setLat={setLat}
+              setLng={setLng}
+              lat={lat}
+              lng={lng}
+            />
+            <Button
+              variant="contained"
+              color="secondary"
+              sx={{
+                m: 1,
+                height: "40px",
+                width: "45%",
+              }}
+            >
+              Mark as taken
+            </Button>
+
+            <Button
+              variant="contained"
+              color="secondary"
+              onClick={navigateToItem}
+              // component={RouterLink}
+              // to="/map"
+              sx={{
+                m: 1,
+                height: "40px",
+                width: "45%",
+              }}
+            >
+              Show on map
+            </Button>
+          </div>
+        </Box>
+      </div>
+      {/* Recommendations */}
+      <div>
+        <Typography>You might also like:</Typography>
+        <Box sx={{ p: 5, flexDirection: "column" }} width="100%">
+          <div key={chosenCategory.categoryId}>
+            <Typography variant="h5" sx={{ fontWeight: "bold", mb: 1 }}>
+              {chosenCategory.title}
+            </Typography>
+            <Typography variant="h6" sx={{ fontWeight: "bold", mb: 1 }}>
+              Description
+            </Typography>
+            <Typography variant="body1" sx={{ mb: 1 }}>
+              {chosenCategory.description}
+            </Typography>
+          </div>
+        </Box>
+      </div>
     </div>
   );
 };
