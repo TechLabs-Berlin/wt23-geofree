4612900aa940279265ea1186722889eb25044859
JoannaKowalik <joanna.kowalik@icloud.com>
2023-04-04 09:01:33 +0200

    eslint bug fixed

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { Box, Grid, Card, Typography } from "@mui/material";
 import { CardActionArea } from "@mui/material";
 import { useNavigate } from "react-router-dom";
